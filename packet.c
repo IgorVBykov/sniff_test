962ced853dbbb1638cefd3f920fa757c325b0d16
Igor Bykov <igor.v.bykov@gmail.com>
2017-03-22 09:25:27 +0600

    Deleting comments

--- packet.c
+++ packet.c
@@ -1,37 +1,37 @@
 #include "packet.h"
-
 void handlePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
     const struct EthernetFrame *ethernet;
-    struct IPPacket *ip;
-    struct TCPPacket *tcp;
+    const struct IPPacket *ip;
+    const struct TCPPacket *tcp;
     const struct UDPPacket *udp;
     const u_char *payload;
     const u_char* ch;
+    u_short* preset;
 
-    u_int16_t crc16;
+    u_int16_t checksum;
 
-    size_t ipHDRSize, ipPacketSize, tcpPayloadSize, udpPacketSize;
-    int tcpHDRSize;
-    int payloadSize;
+    size_t ipHDRSize, ipPacketSize;
+    size_t tcpHDRSize;
+    size_t payloadSize;
 
     struct  in_addr* ptpa;
     struct  in_addr* pspa;
 
     ethernet = (struct EthernetFrame*)(packet);
-    pspa = (struct in_addr*)(packet + ETHERNET_HDR_SIZE + 14);
-    ptpa = (struct in_addr*)(packet + ETHERNET_HDR_SIZE + 24);
+    pspa = (struct in_addr*)(packet + ETHERNET_HDR_SIZE + ARP_SPA_OFFSET);
+    ptpa = (struct in_addr*)(packet + ETHERNET_HDR_SIZE + ARP_TPA_OFFSET);
     //  Выводим ARP и RARP
     if((ntohs(ethernet->ether_type) == ETHERTYPE_ARP)||(ntohs(ethernet->ether_type) == ETHERTYPE_RARP)) {
-        printf("ARP(RARP) packet.\n");
+        printf("ARP(RARP) packet:\n");
         printf("SPA: %s\n", inet_ntoa(*pspa));
         printf("TPA: %s\n", inet_ntoa(*ptpa));
         payload = (u_char *)(packet);
         payloadSize = ETHERNET_HDR_SIZE + ARP_LENGTH;
-        printf("   Payload (%d bytes):\n", payloadSize);
+        printf("   Payload (%d bytes):\n", (int)payloadSize);
         printPayload(payload, payloadSize + FCS_SIZE + 20);
     }
-    /* define/compute ip header offset */
+    // Вычисляем смещение IP-пакета
     ip = (struct IPPacket*)(packet + ETHERNET_HDR_SIZE);
     ipHDRSize = IP_HL(ip)*4;
     ipPacketSize = ntohs(ip->ip_len);
@@ -39,14 +39,15 @@ void handlePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *
         printf("Invalid IP header length: %u bytes\n", (unsigned int)ipHDRSize);
         return;
     }
+    payload = (u_char *)(packet);
+    payloadSize = ETHERNET_HDR_SIZE + ipPacketSize;
     switch(ip->ip_p) {
         case IPPROTO_TCP:
             printf("TCP:\n");
             tcp = (struct TCPPacket*)(packet + ETHERNET_HDR_SIZE + ipHDRSize);
             tcpHDRSize = TH_OFF(tcp)*4;
-            tcpPayloadSize = ipPacketSize - (ipHDRSize + tcpHDRSize);
             if (tcpHDRSize < 20) {
-                printf("Invalid TCP header length: %u bytes\n", tcpHDRSize);
+                printf("Invalid TCP header length: %u bytes\n", (unsigned int)tcpHDRSize);
                 return;
             }
             printf("IP packet size: ");
@@ -65,50 +66,38 @@ void handlePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *
                 ++ch;
             }
             printf("\n");
-            ip->ip_sum = 0;
+            preset = &ip->ip_sum;
+            *preset = 0;
             printf("Calculated IP CRC: ");
-            crc16 = calcIP_CRC(packet);
-            ch = (const u_char*)&crc16;
+            checksum = calcIP_CRC(packet);
+            ch = (const u_char*)&checksum;
             printf("%02x ", *ch++); printf("%02x\n", *ch);
-            tcp->th_sum = 0;
+            preset = &tcp->th_sum;
+            *preset = 0;
             printf("Calculated TCP CRC: ");
-            crc16 = calcTCP_CRC(packet);
-            ch = (const u_char*)&crc16;
+            checksum = calcTCP_CRC(packet);
+            ch = (const u_char*)&checksum;
             printf("%02x ", *ch++); printf("%02x\n", *ch);
-            payload = (u_char *)(packet);
-            payloadSize = ETHERNET_HDR_SIZE + ipPacketSize;
             printf("From: %s\n", inet_ntoa(ip->ip_src));
             printf("To: %s\n", inet_ntoa(ip->ip_dst));
             printf("Src port: %d\n", ntohs(tcp->th_sport));
             printf("Dst port: %d\n", ntohs(tcp->th_dport));
-            if (payloadSize > 0) {
-//                printf("Frame size (%d bytes):\n", payloadSize);
+            if (payloadSize) {
                 printPayload(payload, payloadSize);
-//                printf("Ethernet header(%d): \n\n", ETHERNET_HDR_SIZE);
-//                printFrameFragment(payload, ETHERNET_HDR_SIZE);
-//                printf("IP header(%d): \n", (int)ipHDRSize);
-//                printFrameFragment(payload + ETHERNET_HDR_SIZE, ipHDRSize);
-//                printf("TCP header(%d): \n", tcpHDRSize);
-//                printFrameFragment(payload + ETHERNET_HDR_SIZE + ipHDRSize, tcpHDRSize);
-//                printf("TCP payload(%d): \n", (int)tcpPayloadSize);
-//                printFrameFragment(payload + ETHERNET_HDR_SIZE + ipHDRSize + tcpHDRSize, tcpPayloadSize);
-//                printf("Ethernet FCS(4): \n");
-//                printFrameFragment(payload + ETHERNET_HDR_SIZE + ipHDRSize + tcpHDRSize + tcpPayloadSize, FCS_SIZE);
             }
             else {
                 printf("No payload.\n");
             }
             return;
         case IPPROTO_UDP:
-            udp = (struct UDPPacket*)(packet + ETHERNET_HDR_SIZE + ipHDRSize);
-            udpPacketSize = ntohs(udp->uh_length);
+            return;
             printf("UDP:\n");
             printf("From: %s\n", inet_ntoa(ip->ip_src));
             printf("To: %s\n", inet_ntoa(ip->ip_dst));
             printf("Src port: %d\n", ntohs(udp->uh_sport));
             printf("Dst port: %d\n", ntohs(udp->uh_dport));
-            if (payloadSize > 0) {
-                printPayload((u_char *)udp, udpPacketSize);
+            if (payloadSize) {
+                printPayload(payload, payloadSize);
             }
             else {
                 printf("No payload.\n");
@@ -116,10 +105,11 @@ void handlePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *
             return;
         case IPPROTO_ICMP:
             printf("ICMP:\n");
-            payload = (const u_char*)(packet + ETHERNET_HDR_SIZE + ipHDRSize);
-            payloadSize = ipPacketSize - ipHDRSize;
             if(payloadSize) {
                 printPayload(payload, payloadSize);
+            }
+            else {
+                printf("No payload.\n");
             }
             return;
         case IPPROTO_IP:
@@ -128,7 +118,7 @@ void handlePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *
             printf("To: %s\n", inet_ntoa(ip->ip_dst));
             payloadSize = ipPacketSize - ipHDRSize;
             payload = (u_char *)(packet + ETHERNET_HDR_SIZE + ipHDRSize);
-            if(payloadSize > 0) {
+            if(payloadSize) {
                 printPayload(payload, payloadSize);
             }
             return;
@@ -137,17 +127,6 @@ void handlePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *
             return;
     }
     return;
-}
-
-void printFrameFragment(const u_char *payload, int len)
-{
-    const u_char *ch;
-    ch = payload;
-    for(int i = 0; i < len; ++i) {
-        printf("%02x ", *ch);
-        ++ch;
-    }
-    printf("\n\n");
 }
 
 void printPayload(const u_char *payload, int len)
@@ -240,9 +219,6 @@ u_int16_t calcCRC(const u_int16_t *packet, int length)
     uint32_t cksum = 0;
     while(length > 1)
     {
-//        cksum += ntohs(*packet);
-//        ++packet;
-//        length -=sizeof(u_int16_t);
         cksum += *packet++;
         length -= 2;
     }
@@ -257,6 +233,8 @@ u_int16_t calcCRC(const u_int16_t *packet, int length)
 
 u_int16_t calcTCP_CRC(const u_char *packet)
 {
+    char tcpBuf[65536];
+    //memset(tcpBuf, 0, 5000);
     uint16_t csum;
     size_t ipHDRSize;
     size_t ipPacketSize;
@@ -268,14 +246,12 @@ u_int16_t calcTCP_CRC(const u_char *packet)
     ipHDRSize = IP_HL(ip)*4;
     ipPacketSize = ntohs(ip->ip_len);
     ipPayloadSize = ipPacketSize - ipHDRSize;
-    tcp = (const struct TCPPacket *)((const u_char *)ip + ipHDRSize);
+    tcp = (const struct TCPPacket *)(packet + ETHERNET_HDR_SIZE + ipHDRSize);//(packet + ETHERNET_HDR_SIZE + ipHDRSize);
     psdHeader.ip_src = ip->ip_src;
     psdHeader.ip_dst = ip->ip_dst;
     psdHeader.zeroes = 0;
     psdHeader.protocol = IPPROTO_TCP;
     psdHeader.length = htons(ipPayloadSize);
-
-    char tcpBuf[65536];
     memcpy(tcpBuf, &psdHeader, PSD_HEADER_LENGTH);
     memcpy(tcpBuf + PSD_HEADER_LENGTH, tcp, ipPayloadSize);
     csum = calcCRC((uint16_t *)tcpBuf, ipPayloadSize + PSD_HEADER_LENGTH);
